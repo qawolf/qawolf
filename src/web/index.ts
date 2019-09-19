19f769528191c822d7589377166fa1092bef2d26
Jon Perl <perl.jonathan@gmail.com>
2019-09-19 13:30:30 -0600

    rollup socketio with web

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,14 +1,24 @@
+import io from "socket.io-client";
 import * as actions from "./actions";
 import { Executor } from "./Executor";
 
 const qawolf = {
   actions,
-  Executor
+  Executor,
+  io
 };
 
 if (typeof window !== "undefined") {
   (window as any).qawolf = qawolf;
   console.log("loaded qawolf");
+
+  const socket: any = io("http://localhost:3000", {
+    transports: ["websocket"]
+  });
+
+  socket.on("connect", () => {
+    console.log("CONNECTED ON CLIENT");
+  });
 }
 
 export default qawolf;
