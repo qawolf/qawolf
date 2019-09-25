b2a29d22d63ff5bdfcb083590d48acb010fdacc6
Jon Perl <perl.jonathan@gmail.com>
2019-09-25 12:03:45 -0600

    add puppeteer dependency

---
+++ src/temp.ts
@@ -0,0 +1,10 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();
