d2668494bd95b0379412ba3b4972fc8545599875
Jon Perl <perl.jonathan@gmail.com>
2019-10-01 17:07:50 -0600

    move cli.ts to cli/index.ts

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,12 +3,12 @@
 import clear from "clear";
 import program from "commander";
 import fs from "fs-extra";
-import { BrowserRunner } from "./browser/BrowserRunner";
-import { renderCli } from "./callbacks/cli";
-import { buildScreenshotCallback } from "./callbacks/screenshot";
-import { loginJob } from "./fixtures/job";
-import { logger } from "./logger";
-import { planJob } from "./planner";
+import { BrowserRunner } from "../browser/BrowserRunner";
+import { renderCli } from "../callbacks/cli";
+import { buildScreenshotCallback } from "../callbacks/screenshot";
+import { loginJob } from "../fixtures/job";
+import { logger } from "../logger";
+import { planJob } from "../planner";
 
 clear();
 
