77edb58ad51a0b17e4fa9e044ba360f55d02f18a
Jon Perl <perl.jonathan@gmail.com>
2019-10-14 14:57:33 -0600

    prefix env variables with QAW_
    resolves https://github.com/qawolf/qawolf/issues/61
    
    - remove unused env variables in config for now

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
-const dotEnvPath = process.env.DOTENV_PATH;
+const dotEnvPath = process.env.QAW_DOTENV_PATH;
 dotenv.config(dotEnvPath ? { path: dotEnvPath } : {});
 
-const testUrl = process.env.TEST_URL || "http://localhost:5000/";
+const testUrl = process.env.QAW_TEST_URL || "http://localhost:5000/";
 
 const parseBool = (value: string | undefined) => {
   return !!value && value !== "false";
@@ -16,16 +16,11 @@ const parseNumber = (value: string | undefined, defaultValue: number = 0) => {
 };
 
 export const CONFIG = {
-  chromeExecutablePath: process.env.CHROME_EXECUTABLE_PATH,
-  dataAttribute: process.env.DATA_ATTRIBUTE || null,
-  headless: parseBool(process.env.HEADLESS),
-  keepBrowserOpen: parseBool(process.env.KEEP_BROWSER_OPEN),
-  locatorTimeoutMs: parseNumber(process.env.locatorTimeoutMs, 30000),
-  logLevel: process.env.LOG_LEVEL,
-  logPath: process.env.LOG_PATH,
-  saveBrowserWsPath: process.env.SAVE_BROWSER_WS_PATH,
-  screenshotPath: process.env.SCREENSHOT_PATH,
-  sleepAfterEach: parseNumber(process.env.SLEEP_AFTER_EACH),
-  testUrl,
-  useLocalModule: parseBool(process.env.USE_LOCAL_MODULE)
+  chromeExecutablePath: process.env.QAW_CHROME_EXECUTABLE_PATH,
+  dataAttribute: process.env.QAW_DATA_ATTRIBUTE || null,
+  headless: parseBool(process.env.QAW_HEADLESS),
+  locatorTimeoutMs: parseNumber(process.env.QAW_LOCATOR_TIMEOUT_MS, 30000),
+  logLevel: process.env.QAW_LOG_LEVEL,
+  logPath: process.env.QAW_LOG_PATH,
+  testUrl
 };
