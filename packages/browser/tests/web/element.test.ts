f528a71e377a631f638f5b0ea69d1861a9830693
Laura Cressman <lauracressman@mac.com>
2019-10-16 11:54:00 -0600

    ABC tests in element

--- packages/browser/tests/web/element.test.ts
+++ packages/browser/tests/web/element.test.ts
@@ -60,114 +60,6 @@ describe("getDataValue", () => {
   });
 });
 
-describe("getLabels", () => {
-  it("correctly returns labels", async () => {
-    const nullLabels = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getLabels(document.getElementsByTagName("h2")[0]);
-    });
-
-    expect(nullLabels).toBeNull();
-
-    const usernameLabels = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getLabels(
-        document.getElementsByTagName("input")[0]
-      );
-    });
-
-    expect(usernameLabels).toEqual(["username"]);
-  });
-});
-
-describe("getParentText", () => {
-  it("correctly returns parent text", async () => {
-    const iconParentText = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getParentText(
-        document.getElementsByTagName("i")[0]
-      );
-    });
-
-    expect(iconParentText).toEqual(["login", "login"]);
-  });
-});
-
-describe("getPlaceholder", () => {
-  it("returns placeholder if present", async () => {
-    const placeholder = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-      const input = document.getElementsByTagName("input")[0];
-      input.placeholder = "enter username";
-
-      const result = qawolf.element.getPlaceholder(
-        document.getElementsByTagName("input")[0]
-      );
-
-      input.removeAttribute("placeholder");
-
-      return result;
-    });
-
-    expect(placeholder).toBe("enter username");
-  });
-
-  it("returns null if no placeholder", async () => {
-    const nullPlaceholder = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getPlaceholder(
-        document.getElementsByTagName("input")[0]
-      );
-    });
-
-    expect(nullPlaceholder).toBeNull();
-  });
-
-  it("returns disabled option text for select", async () => {
-    await page.goto(`${CONFIG.testUrl}dropdown`);
-
-    const placeholder = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getPlaceholder(
-        document.getElementsByTagName("select")[0]
-      );
-    });
-
-    expect(placeholder).toBe("please select an option");
-
-    await page.goto(`${CONFIG.testUrl}login`);
-  });
-});
-
-describe("getTextContent", () => {
-  it("returns text content", async () => {
-    const headerTextContent = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getTextContent(
-        document.getElementsByTagName("h2")[0]
-      );
-    });
-
-    expect(headerTextContent).toBe("login page");
-
-    const nullTextContent = await page.evaluate(() => {
-      const qawolf: QAWolfWeb = (window as any).qawolf;
-
-      return qawolf.element.getTextContent(
-        document.getElementsByTagName("input")[0]
-      );
-    });
-
-    expect(nullTextContent).toBeNull();
-  });
-});
-
 describe("getDescriptor", () => {
   it("correctly returns full element element", async () => {
     const inputDescriptor = await page.evaluate(() => {
@@ -279,5 +171,113 @@ describe("getIconContent", () => {
     });
 
     expect(iconContent).toBeNull();
+  });
+});
+
+describe("getLabels", () => {
+  it("correctly returns labels", async () => {
+    const nullLabels = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getLabels(document.getElementsByTagName("h2")[0]);
+    });
+
+    expect(nullLabels).toBeNull();
+
+    const usernameLabels = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getLabels(
+        document.getElementsByTagName("input")[0]
+      );
+    });
+
+    expect(usernameLabels).toEqual(["username"]);
+  });
+});
+
+describe("getParentText", () => {
+  it("correctly returns parent text", async () => {
+    const iconParentText = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getParentText(
+        document.getElementsByTagName("i")[0]
+      );
+    });
+
+    expect(iconParentText).toEqual(["login", "login"]);
+  });
+});
+
+describe("getPlaceholder", () => {
+  it("returns placeholder if present", async () => {
+    const placeholder = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+      const input = document.getElementsByTagName("input")[0];
+      input.placeholder = "enter username";
+
+      const result = qawolf.element.getPlaceholder(
+        document.getElementsByTagName("input")[0]
+      );
+
+      input.removeAttribute("placeholder");
+
+      return result;
+    });
+
+    expect(placeholder).toBe("enter username");
+  });
+
+  it("returns null if no placeholder", async () => {
+    const nullPlaceholder = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getPlaceholder(
+        document.getElementsByTagName("input")[0]
+      );
+    });
+
+    expect(nullPlaceholder).toBeNull();
+  });
+
+  it("returns disabled option text for select", async () => {
+    await page.goto(`${CONFIG.testUrl}dropdown`);
+
+    const placeholder = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getPlaceholder(
+        document.getElementsByTagName("select")[0]
+      );
+    });
+
+    expect(placeholder).toBe("please select an option");
+
+    await page.goto(`${CONFIG.testUrl}login`);
+  });
+});
+
+describe("getTextContent", () => {
+  it("returns text content", async () => {
+    const headerTextContent = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getTextContent(
+        document.getElementsByTagName("h2")[0]
+      );
+    });
+
+    expect(headerTextContent).toBe("login page");
+
+    const nullTextContent = await page.evaluate(() => {
+      const qawolf: QAWolfWeb = (window as any).qawolf;
+
+      return qawolf.element.getTextContent(
+        document.getElementsByTagName("input")[0]
+      );
+    });
+
+    expect(nullTextContent).toBeNull();
   });
 });
