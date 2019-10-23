a064a9d5af608123de0f1a190bcd10142f78160a
Laura Cressman <lauracressman@mac.com>
2019-10-23 14:28:17 -0600

    Create github package

--- packages/github/src/index.ts
+++ packages/github/src/index.ts
@@ -1,1 +1,18 @@
-console.log("TODO");
+import Octokit from "@octokit/rest";
+
+const octokit = new Octokit({
+  auth: "d9e1b19607b2b07ed5c6f2e78fa5fb1bd3cabf26"
+});
+
+const main = async () => {
+  const res = await octokit.checks.create({
+    owner: "qawolf",
+    repo: "internal",
+    name: "QA Wolf",
+    head_sha: "9c52a7a6f2090c58bcb23ae066ce31920789f71c"
+  });
+
+  console.log("RESPONSE", res);
+};
+
+main();
