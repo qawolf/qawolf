61094162d37e63be7d3aacc92a8d19809def2e80
Laura Cressman <lauracressman@mac.com>
2019-10-24 10:23:36 -0600

    Add test for running multiple tests

--- packages/cli/tests/runTest.test.ts
+++ packages/cli/tests/runTest.test.ts
@@ -33,4 +33,32 @@ describe("runTest", () => {
       "demogorgon!"
     );
   });
+
+  it("returns results when running multiple tests", async () => {
+    const results = await runTest("u");
+
+    expect(results.numFailedTests).toBe(1);
+    expect(results.numPassedTests).toBe(5);
+    expect(results.numPendingTests).toBe(1);
+
+    const failureResults = results.testResults.find(result =>
+      result.testFilePath.includes("failure")
+    );
+    const successResults = results.testResults.find(result =>
+      result.testFilePath.includes("success")
+    );
+
+    expect(failureResults!.testResults).toMatchObject([
+      { status: "passed", title: "step 1" },
+      { status: "passed", title: "step 2" },
+      { status: "failed", title: "step 3" },
+      { status: "pending", title: "step 4" }
+    ]);
+
+    expect(successResults!.testResults).toMatchObject([
+      { status: "passed", title: "step 1" },
+      { status: "passed", title: "step 2" },
+      { status: "passed", title: "step 3" }
+    ]);
+  });
 });
