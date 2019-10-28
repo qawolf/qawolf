185eee4db8ca2ff318ad35455864af9d63e63329
Jon Perl <perl.jonathan@gmail.com>
2019-10-27 18:17:53 -0600

    speed up gif

--- packages/screen/src/createGif.ts
+++ packages/screen/src/createGif.ts
@@ -19,7 +19,7 @@ export const createGif = (options: CreateGifOptions) => {
     const ffmpeg = spawn("sh", [
       "-c",
       // https://superuser.com/a/556031/856890
-      `ffmpeg -i ${options.videoPath} -vf "fps=10,scale=${shrunkHeight}:-1:flags=lanczos" -c:v pam -f image2pipe - | convert -delay 10 - -loop 0 -layers optimize ${options.gifPath}`
+      `ffmpeg -i ${options.videoPath} -vf "fps=10,scale=${shrunkHeight}:-1:flags=lanczos,setpts=0.5*PTS" -c:v pam -f image2pipe - | convert -delay 10 - -loop 0 -layers optimize ${options.gifPath}`
     ]);
 
     ffmpeg.on("close", () => {
