a1ba8f1a57fdfb160ab9df696d71dbc85115296e
Jon Perl <perl.jonathan@gmail.com>
2019-10-28 15:31:38 -0600

    retry 3

--- packages/cli/src/runTest.ts
+++ packages/cli/src/runTest.ts
@@ -11,7 +11,7 @@ export const runTest = async (
   const setupFailFast = path.resolve(path.dirname(modulePath), "./setup.js");
 
   const jestConfig: any = {
-    // 2
+    // 3
     clearCache: true,
     config: "{}",
     // assume .qawolf is relative to the current working directory
