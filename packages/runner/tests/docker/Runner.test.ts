b0aabda094b9dca744b10a956deda6bccddee7f6
Jon Perl <perl.jonathan@gmail.com>
2019-11-03 14:24:34 -0700

    fix runner test

--- packages/runner/tests/docker/Runner.test.ts
+++ packages/runner/tests/docker/Runner.test.ts
@@ -6,6 +6,7 @@ import { loginWorkflow } from "../../../build-workflow/fixtures/loginWorkflow";
 import { Runner } from "../../src/Runner";
 
 it("records dom replayer and a video", async () => {
+  CONFIG.domPath = await makeTempDir();
   CONFIG.videoPath = await makeTempDir();
 
   const runner = await Runner.create({
@@ -16,9 +17,9 @@ it("records dom replayer and a video", async () => {
   await sleep(500);
   await runner.close();
 
-  const savePath = `${CONFIG.videoPath}/${loginWorkflow.name}`;
-  expect(await pathExists(`${savePath}/video.mp4`)).toBeTruthy();
-  expect(await pathExists(`${savePath}/video.gif`)).toBeTruthy();
+  const videoPath = `${CONFIG.videoPath}/${loginWorkflow.name}`;
+  expect(await pathExists(`${videoPath}/video.mp4`)).toBeTruthy();
+  expect(await pathExists(`${videoPath}/video.gif`)).toBeTruthy();
   await remove(CONFIG.videoPath);
 
   // 668 instead of 667 since it rounds up to even numbers
@@ -27,5 +28,6 @@ it("records dom replayer and a video", async () => {
     width: 376
   });
 
-  expect(await pathExists(`${savePath}/page_0.html`)).toBeTruthy();
+  const domPath = `${CONFIG.domPath}/${loginWorkflow.name}`;
+  expect(await pathExists(`${domPath}/page_0.html`)).toBeTruthy();
 });
