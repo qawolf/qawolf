4fdb23c63814a860faa8659e3b5cc4fd0e3a1876
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-11-11 23:29:54 +0530

    fix: lint

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,10 +8,9 @@ import { githubAction } from "./githubAction";
 import { record } from "./record";
 import { test } from "./test";
 import { parseUrl } from "./utils";
-const { version } = require('./package');
+const { version } = require("./package");
 
-program
-  .version(version);
+program.version(version);
 
 let recordCommand = program
   .command("record <url> [name]")
