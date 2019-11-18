4ef054e64bb420eeaed999ff54573e1e58779048
Jon Perl <perl.jonathan@gmail.com>
2019-11-18 12:29:00 -0700

    cli: add "qawolf azure" and "qawolf circleci" commands
    - remove unused @qawolf/github package for now

--- packages/github/tests/buildAction.test.ts
+++
@@ -1,9 +0,0 @@
-import { buildAction } from "../src/buildAction";
-
-describe("buildAction", () => {
-  it("returns action template", () => {
-    const action = buildAction();
-
-    expect(action).toMatchSnapshot();
-  });
-});
