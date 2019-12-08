fc1e9478d0484c2499d408c3fba5272f3199fdcf
Jon Perl <perl.jonathan@gmail.com>
2019-12-08 10:30:04 -0700

    qawolf: only import cli if running in CLI

--- packages/qawolf/src/index.ts
+++ packages/qawolf/src/index.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-// run cli
-import "@qawolf/cli";
+const isCLI = !module.parent;
+if (isCLI) {
+  require("@qawolf/cli");
+}
 
 // export public API
 export { Browser, launch, LaunchOptions, Page } from "@qawolf/browser";
