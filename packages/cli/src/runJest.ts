658db501d66b5d0d87dee5af61ae5fc06a32de06
Jon Perl <perl.jonathan@gmail.com>
2020-01-06 11:24:51 -0700

    add clarifying comments to runJest

--- packages/cli/src/runJest.ts
+++ packages/cli/src/runJest.ts
@@ -12,16 +12,19 @@ export const runJest = (args: string[] = []) => {
     // assume .qawolf is relative to the current working directory
     roots: ["<rootDir>/.qawolf"],
     // run with fast fail since we do not want to continue e2e tests when one fails
+    // this will not be necessary after Jest Circus fixes --bail
     setupFilesAfterEnv: [setupFailFast],
     // override transform to prevent using external babel-jest
     transform: {}
   });
 
   let command = `npx jest --testTimeout=60000 --config='${config}'`;
+  // pass through other arguments to jest
   if (args.length) {
     command += ` ${args.join(" ")}`;
   }
 
+  // log the command we run to make it clear this is an alias for npx jest
   console.log(command + "\n");
 
   try {
