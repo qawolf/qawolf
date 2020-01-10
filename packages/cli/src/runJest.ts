fd2c7b8c99037ad967853afda99fe04429f596d6
Jon Perl <perl.jonathan@gmail.com>
2020-01-10 14:47:08 -0700

    move fail fast configuration to a preset @qawolf/jest-fail-fast

--- packages/cli/src/runJest.ts
+++ packages/cli/src/runJest.ts
@@ -1,4 +1,3 @@
-import path from "path";
 import { execSync } from "child_process";
 
 export const runJest = (args: string[] = []) => {
@@ -6,16 +5,9 @@ export const runJest = (args: string[] = []) => {
    * Returns exit code. 0 for success, 1 for failed.
    */
 
-  // make it relative to the current working directory
-  // so the command is shorter when logged
-  const config = path.relative(
-    process.cwd(),
-    path.join(__dirname, "failFast.json")
-  );
-
   // use a config file instead of json because escaping
   // the json for different shells is too difficult
-  let command = `npx jest --config=${config} --rootDir=.qawolf --testTimeout=60000`;
+  let command = `npx jest --preset="@qawolf/jest-fail-fast" --rootDir=.qawolf --testTimeout=60000`;
 
   // pass through other arguments to jest
   if (args.length) {
