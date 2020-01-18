b197667299c55505ffd7c203c95f7d96cf9897a4
Jon Perl <perl.jonathan@gmail.com>
2020-01-18 07:21:08 -0700

    add log before and after click

--- packages/browser/src/actions/clickElement.ts
+++ packages/browser/src/actions/clickElement.ts
@@ -2,7 +2,7 @@ import { logger } from "@qawolf/logger";
 import { ElementHandle } from "puppeteer";
 
 export const clickElement = async (element: ElementHandle): Promise<void> => {
-  logger.verbose("clickElement");
-
+  logger.verbose("clickElement: received");
   await element.click();
+  logger.verbose("clickElement: clicked");
 };
