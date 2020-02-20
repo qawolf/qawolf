632e46026574b558c1b511874976bed66176fa7d
Jon Perl <perl.jonathan@gmail.com>
2020-02-20 11:11:45 -0700

    port BrowserContext tests to sandbox
    - compare page index instead of page objects

--- packages/browser/tests/context/BrowserContext.test.ts
+++ packages/browser/tests/context/BrowserContext.test.ts
@@ -3,7 +3,7 @@ import { launch } from "../../src/context/launch";
 
 describe("BrowserContext.close", () => {
   it("closes the parent Browser", async () => {
-    const context = await launch({ url: CONFIG.testUrl });
+    const context = await launch({ url: CONFIG.sandboxUrl });
     expect(context.browser().isConnected()).toEqual(true);
     await context.close();
     expect(context.browser().isConnected()).toEqual(false);
@@ -12,30 +12,29 @@ describe("BrowserContext.close", () => {
 
 describe("BrowserContext.page", () => {
   it("waits for a page to open", async () => {
-    const context = await launch({ url: CONFIG.testUrl });
+    const context = await launch({ url: CONFIG.sandboxUrl });
 
     const pagePromise = context.page({ page: 1 });
-
-    const pageOne = await context.newPage();
-    expect(await pagePromise).toEqual(pageOne);
+    await context.newPage();
+    expect((await pagePromise).qawolf().index()).toEqual(1);
 
     await context.close();
   });
 
   it("chooses the first open page if the current page is closed", async () => {
-    const context = await launch({ url: CONFIG.testUrl });
+    const context = await launch({ url: CONFIG.sandboxUrl });
 
     const pageOne = await context.newPage();
 
     // change the current page to 1
     await context.page({ page: 1 });
     let currentPage = await context.page();
-    expect(currentPage).toEqual(pageOne);
+    expect(currentPage.qawolf().index()).toEqual(1);
 
     await pageOne.close();
 
     currentPage = await context.page();
-    expect(currentPage).not.toEqual(pageOne);
+    expect(currentPage.qawolf().index()).toEqual(0);
 
     await context.close();
   });
@@ -43,9 +42,9 @@ describe("BrowserContext.page", () => {
 
 describe("BrowserContext.find", () => {
   it("finds an element", async () => {
-    const context = await launch({ url: `${CONFIG.testUrl}login` });
+    const context = await launch({ url: `${CONFIG.sandboxUrl}buttons` });
 
-    const elementHandle = await context.find({ css: "#login > button" });
+    const elementHandle = await context.find({ css: "#html-button" });
     expect(elementHandle).toBeTruthy();
     expect(
       await elementHandle!.evaluate(e => (e as HTMLButtonElement).type)
