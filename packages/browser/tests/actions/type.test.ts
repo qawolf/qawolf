fe8fca154563c2b0ae4964f957097f451e715738
Jon Perl <perl.jonathan@gmail.com>
2020-02-20 09:22:18 -0700

    Reorganize type tests

--- packages/browser/tests/actions/type.test.ts
+++ packages/browser/tests/actions/type.test.ts
@@ -11,73 +11,8 @@ beforeAll(async () => {
 
 afterAll(() => context.close());
 
-describe("BrowserContext.type", () => {
-  it("sets text in input[type=text]", async () => {
-    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
-
-    const element = await context.type(
-      { css: '[data-qa="html-text-input"]' },
-      "spirit"
-    );
-
-    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
-      "spirit"
-    );
-  });
-});
-
-describe("Page.type", () => {
-  it("appends text to input[type=text]", async () => {
-    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
-
-    const element = await page
-      .qawolf()
-      .type({ css: '[data-qa="html-text-input-filled"]' }, " more");
-
-    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
-      "initial text more"
-    );
-  });
-
-  it("replaces text in input[type=text]", async () => {
-    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
-
-    const element = await page
-      .qawolf()
-      .type({ css: '[data-qa="html-text-input-filled"]' }, "spirit", {
-        replace: true
-      });
-
-    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
-      "spirit"
-    );
-  });
-
-  it("clears input[type=text] for null", async () => {
-    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
-
-    const element = await page
-      .qawolf()
-      .type({ css: '[data-qa="html-text-input-filled"]' }, null, {
-        replace: true
-      });
-
-    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe("");
-  });
-
-  it("sets a input[type=number] value", async () => {
-    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
-
-    const element = await page
-      .qawolf()
-      .type({ css: '[data-qa="html-number-input"]' }, "999");
-
-    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
-      "999"
-    );
-  });
-
-  it("replaces a content editables text", async () => {
+describe("content editable", () => {
+  it("replaces text", async () => {
     await context.goto(`${CONFIG.sandboxUrl}content-editables`);
 
     const element = await page
@@ -90,8 +25,10 @@ describe("Page.type", () => {
       "spirit"
     );
   });
+});
 
-  it("replaces a input[type=date] value", async () => {
+describe("input[type=date] ", () => {
+  it("replaces the value", async () => {
     await context.goto(`${CONFIG.sandboxUrl}date-pickers`);
 
     const element = await page
@@ -108,8 +45,73 @@ describe("Page.type", () => {
       expected
     );
   });
+});
 
-  it("replaces a input[type=time] value", async () => {
+describe("input[type=number] ", () => {
+  it("sets value", async () => {
+    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
+
+    const element = await page
+      .qawolf()
+      .type({ css: '[data-qa="html-number-input"]' }, "999");
+
+    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
+      "999"
+    );
+  });
+});
+
+describe("input[type=text]", () => {
+  test("appends text", async () => {
+    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
+
+    // test context.type wrapper works
+    let element = await context.type(
+      { css: '[data-qa="html-text-input"]' },
+      "spirit"
+    );
+    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
+      "spirit"
+    );
+
+    element = await page
+      .qawolf()
+      .type({ css: '[data-qa="html-text-input-filled"]' }, " more");
+
+    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
+      "initial text more"
+    );
+  });
+
+  it("clears text", async () => {
+    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
+
+    const element = await page
+      .qawolf()
+      .type({ css: '[data-qa="html-text-input-filled"]' }, null, {
+        replace: true
+      });
+
+    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe("");
+  });
+
+  it("replaces text", async () => {
+    await context.goto(`${CONFIG.sandboxUrl}text-inputs`);
+
+    const element = await page
+      .qawolf()
+      .type({ css: '[data-qa="html-text-input-filled"]' }, "spirit", {
+        replace: true
+      });
+
+    expect(await element.evaluate((e: HTMLInputElement) => e.value)).toBe(
+      "spirit"
+    );
+  });
+});
+
+describe("input[type=time]  ", () => {
+  it("replaces the value", async () => {
     await context.goto(`${CONFIG.sandboxUrl}time-pickers`);
 
     const element = await page
