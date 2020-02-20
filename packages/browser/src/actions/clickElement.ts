cd49ec259efcbf0c222c69c9871e372524a3fb01
Jon Perl <perl.jonathan@gmail.com>
2020-02-20 14:53:55 -0700

    clickElement: default to not simulate to use Playwright API by default
    This is closer to user behavior too.

--- packages/browser/src/actions/clickElement.ts
+++ packages/browser/src/actions/clickElement.ts
@@ -11,25 +11,26 @@ export const clickElement = async (
 ): Promise<void> => {
   logger.verbose(`clickElement: received ${JSON.stringify(options)}`);
 
-  // ElementHandle.click scrolls an element into view, finds it's coordinates, and clicks on that point.
-  // There are corner cases like custom scrolling where it will break.
-  // https://github.com/puppeteer/puppeteer/issues/2190#issuecomment-380254352
-  // We ran into unexplainable issues with clients where it would hang, but simulating a click works fine.
-  // https://github.com/puppeteer/puppeteer/issues/3347
-  // Since simulating the click is simpler and works more often, we default to it.
-  // However we expose ElementHandle.click behind an option, simulate=false, in case it is needed.
-  // For example, we use simulate=false for the Recorder test since we need a trusted click event.
-  if (options.simulate === false) {
-    await elementHandle.evaluate(element =>
-      console.log("qawolf: click", element)
-    );
-    await elementHandle.click();
-  } else {
+  if (options.simulate) {
+    // We provide options.simulate as a workaround for when Playwright click does not work.
+    // ElementHandle.click scrolls an element into view, finds it's coordinates, and clicks on that point.
+    // There are corner cases like custom scrolling where it will break.
+    // https://github.com/puppeteer/puppeteer/issues/2190#issuecomment-380254352
+    // We ran into unexplainable issues with clients where it would hang, but simulating a click works fine.
+    // https://github.com/puppeteer/puppeteer/issues/3347
+    // However we default to the original Playwright click since there are also scenarios
+    // where simulating a click does not work (ex. Material-UI Dropdown).
+    // This also keeps our API closer to the Playwright API.
     await elementHandle.evaluate((element: HTMLElement) => {
       console.log("qawolf: click", element);
       const clickEvent = new MouseEvent("click", { bubbles: true });
       element.dispatchEvent(clickEvent);
     });
+  } else {
+    await elementHandle.evaluate(element =>
+      console.log("qawolf: click", element)
+    );
+    await elementHandle.click();
   }
 
   logger.verbose("clickElement: clicked");
