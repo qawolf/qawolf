ac604fc3ba41fbfc97aa9c68392dbb6a45bab739
Jon Perl <perl.jonathan@gmail.com>
2020-03-10 15:18:43 -0600

    add scroll e2e test

---
+++ test/e2e/tests/scroll.test.js
@@ -0,0 +1,19 @@
+const qawolf = require('qawolf');
+const { TEST_URL } = require('./utils');
+
+let browser;
+let page;
+
+beforeAll(async () => {
+  browser = await qawolf.launch({ slowMo: 20 });
+  const context = await browser.newContext();
+  await qawolf.register(context);
+  page = await context.newPage();
+});
+
+afterAll(() => browser.close());
+
+test('selects', async () => {
+  await page.goto(`${TEST_URL}infinite-scroll`);
+  await qawolf.scroll(page, 'html', { x: 0, y: 2205 });
+});
