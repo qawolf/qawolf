1ead05ebda795324e1969ec5f5f34a938a658fdd
Jon Perl <perl.jonathan@gmail.com>
2020-04-07 15:02:13 -0600

    refactor test

--- test/cli/test.e2e.test.ts
+++ test/cli/test.e2e.test.ts
@@ -1,4 +1,4 @@
-import { exec } from 'child_process';
+import { exec as execAsync } from 'child_process';
 
 type ExecResult = {
   error: Error | null;
@@ -6,24 +6,25 @@ type ExecResult = {
   stdout: string;
 };
 
+const exec = (command: string): Promise<ExecResult> => {
+  const env = {
+    ...process.env,
+    CI: 'true',
+    FORCE_COLOR: '0',
+  };
+
+  return new Promise((resolve) =>
+    execAsync(command, { env }, (error, stderr, stdout) =>
+      resolve({ error, stderr, stdout }),
+    ),
+  );
+};
+
 describe('test', () => {
   it('runs a test', async () => {
-    const result = await new Promise<ExecResult>((resolve) =>
-      exec(
-        'npx qawolf test',
-        {
-          env: {
-            ...process.env,
-            CI: 'true',
-            FORCE_COLOR: '0',
-          },
-        },
-        (error, stderr, stdout) => resolve({ error, stderr, stdout }),
-      ),
-    );
-
+    const result = await exec('npx qawolf test');
+    expect(result.error).toBeNull();
     expect(result.stderr).toContain('Test: chromium\nnpx jest');
     expect(result.stdout).toContain('PASS test/.qawolf/scroll.test.js');
-    expect(result.error).toBeNull();
   });
 });
