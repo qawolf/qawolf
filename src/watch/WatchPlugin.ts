e340e31943009459c78a02a7a641585bc14ed912
Jon Perl <perl.jonathan@gmail.com>
2020-04-07 18:31:34 -0600

    WatchPlugin: force trigger a test re-run

--- src/watch/WatchPlugin.ts
+++ src/watch/WatchPlugin.ts
@@ -1,6 +1,6 @@
 import { FSWatcher, watch } from 'chokidar';
 import Debug from 'debug';
-import { readFile } from 'fs-extra';
+import { readFile, writeFile } from 'fs-extra';
 import { JestHookSubscriber } from 'jest-watcher';
 import { WatchServer } from './WatchServer';
 
@@ -22,17 +22,8 @@ export class WatchPlugin {
     this._server.on('stopwatch', () => this._stop());
   }
 
-  async _onTestChange(testPath: string): Promise<void> {
-    const newCode = await readFile(testPath, 'utf8');
-    if (newCode === this._code) return;
-
-    debug('test changed');
-    this._code = newCode;
-    this._server.stopTest();
-  }
-
-  async _prepareTest(testPath: string): Promise<void> {
-    debug('prepare test %s', testPath);
+  async _beforeTest(testPath: string): Promise<void> {
+    debug('before test %s', testPath);
 
     // Stop the previous test
     this._server.stopTest();
@@ -41,17 +32,36 @@ export class WatchPlugin {
     await this._server.setEnv(process.env);
 
     // Jest watch will not re-run a test in progress.
-    // To workaround this, we manually watch the test.
-    // When it changes we stop it, which triggers Jest to re-run it.
+    // To workaround this, we watch the test for changes.
+    // When it changes: stop the previous test and trigger the test.
     await this._watch(testPath);
   }
 
-  async _stop(): Promise<void> {
-    this._watcher.close();
+  async _onWatchChange(testPath: string): Promise<void> {
+    const newCode = await readFile(testPath, 'utf8');
+    if (newCode === this._code) {
+      debug('watch change: code did not change');
+      return;
+    }
 
+    debug('watch change: code changed');
+    this._code = newCode;
     await this._server.stopTest();
 
+    // after the previous test is stopped, trigger jest to re-run the test
+    this._triggerTest(testPath);
+  }
+
+  async _stop(): Promise<void> {
+    debug('stop');
+    await Promise.all([this._watcher.close(), this._server.stopTest()]);
     process.exit();
+  }
+
+  async _triggerTest(testPath: string): Promise<void> {
+    debug('trigger test');
+    this._code = await readFile(testPath, 'utf8');
+    await writeFile(testPath, this._code, 'utf8');
   }
 
   async _watch(testPath: string): Promise<void> {
@@ -65,12 +75,12 @@ export class WatchPlugin {
 
     this._code = await readFile(testPath, 'utf8');
 
-    this._watcher.on('change', () => this._onTestChange(testPath));
+    this._watcher.on('change', () => this._onWatchChange(testPath));
   }
 
   public apply(hooks: JestHookSubscriber): void {
     hooks.shouldRunTestSuite(async (suite) => {
-      await this._prepareTest(suite.testPath);
+      await this._beforeTest(suite.testPath);
       return true;
     });
   }
