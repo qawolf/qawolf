b4c298ab2934b35cb175f0a1824eb7c319ed75e8
Jon Perl <perl.jonathan@gmail.com>
2020-04-27 18:33:24 -0600

    serve sandbox fixtures

--- packages/sandbox/bin/serve.js
+++ packages/sandbox/bin/serve.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const { execSync } = require("child_process");
-const { join } = require("path");
+const { execSync } = require('child_process');
+const { join } = require('path');
 
-const buildDir = join(__dirname, "../build");
+const buildDir = join(__dirname, '../build');
 
-const command = `serve -s ${buildDir}`;
+const command = `serve ${buildDir}`;
 console.log(command);
 
-execSync(command, { stdio: "inherit" });
+execSync(command, { stdio: 'inherit' });
