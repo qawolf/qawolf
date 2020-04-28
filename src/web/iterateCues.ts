f07173e40d8c29b8db24df073636b33668bc0fce
Jon Perl <perl.jonathan@gmail.com>
2020-04-28 15:00:55 -0600

    improve iterateCues order

--- src/web/iterateCues.ts
+++ src/web/iterateCues.ts
@@ -21,6 +21,22 @@ export const CueTypesRanked: CueType[] = [
 const include = (cues: Cue[], ...include: CueType[]): Cue[] =>
   cues.filter((cue) => include.includes(cue.type));
 
+const sortByRank = (cues: Cue[]): Cue[] =>
+  cues.sort((a, b) => {
+    const aType = CueTypesRanked.indexOf(a.type);
+    const bType = CueTypesRanked.indexOf(b.type);
+
+    // put the higher rank type at the top
+    if (aType < bType) return -1;
+    if (aType > bType) return 1;
+
+    // put the lower level at the top
+    if (a.level < b.level) return -1;
+    if (a.level > b.level) return 1;
+
+    return 0;
+  });
+
 function* iterateGroups(
   targetCues: Cue[],
   ancestorCue?: Cue,
@@ -41,30 +57,27 @@ function* iterateType(
   const typeTargetCues = include(targetCues, type);
   yield* iterateGroups(typeTargetCues);
 
-  // iterate the ancestor cues of equal or higher rank
-  // with the target cues of the type
+  // iterate the cues of equal or higher rank
   const eligibleTypes = CueTypesRanked.slice(
     // skip attribute since it already iterated
     1,
     CueTypesRanked.indexOf(type) + 1,
   );
+
   for (const ancestorCue of include(ancestorCues, ...eligibleTypes)) {
-    yield* iterateGroups(typeTargetCues, ancestorCue);
+    if (ancestorCue.type === type) {
+      // iterate the ancestor of this type, with all higher ranked target cues
+      yield* iterateGroups(include(targetCues, ...eligibleTypes), ancestorCue);
+    } else {
+      // iterate the ancestor of a higher ranked type, with all of this type target cues
+      yield* iterateGroups(typeTargetCues, ancestorCue);
+    }
   }
 }
 
 export function* iterateCues(cues: Cue[]): Generator<Cue[], void, undefined> {
-  // order by closeness to target
-  const ancestorCues = cues
-    .filter((cue) => cue.level !== 0)
-    .sort((a, b) => a.level - b.level);
-
-  // order by rank
-  const targetCues = cues
-    .filter((cue) => cue.level === 0)
-    .sort(
-      (a, b) => CueTypesRanked.indexOf(a.type) - CueTypesRanked.indexOf(b.type),
-    );
+  const ancestorCues = sortByRank(cues.filter((cue) => cue.level !== 0));
+  const targetCues = sortByRank(cues.filter((cue) => cue.level === 0));
 
   // iterate attribute cues first
   yield* iterateGroups(include(targetCues, 'attribute'));
