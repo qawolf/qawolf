3ddaf0a0e615fe91808170c34fb07b252aee5c7f
Jon Perl <perl.jonathan@gmail.com>
2020-04-29 11:45:39 -0600

    improve isDynamic test

--- test/web/isDynamic.test.ts
+++ test/web/isDynamic.test.ts
@@ -1,62 +1,54 @@
 import { isDynamic } from '../../src/web/isDynamic';
 
-const trueExamples = [
-  '__BVID__71',
-  'StyledBox-sc-13pk1d4-0',
-  'gLFyf',
-  'gb_C',
-  'TSPr2b',
-  'intercom-123v9c3',
-  'u_0_b',
-  'ggWlfB2BMlWvNeAo2F0uqw',
-];
-
-const falseExamples = [
-  'toggle',
-  'destroy',
-  'btn',
-  'mat',
-  'form-control',
-  'b-content__page-input',
-  'glide__bullet',
-  'app',
-  'nav-link',
-  'login-button',
-  'col-sm-12',
-  'footer',
-  'intercom-container-body',
-  'new-todo',
-  'inputtext',
-  'email',
-  'learn-bar',
-  'searchInput',
-  'js-lang-list-button',
-  'central-textlogo__image',
-  'footer-sidebar-icon',
-  'svg',
-  'icon',
-  'fa-search',
-  'slider',
-  'btn-playr-primary',
-  'MyCart__itemCount__label',
-  'SearchBox__cover',
-  'tnt__zipInput',
-  'MyAccount__label',
-  'nav-sprite',
-  'desktop-grid-3',
-  'nytslm-li-link',
-];
-
 describe('isDynamic', () => {
-  it('returns true for dynamic values', () => {
-    trueExamples.forEach((example) => {
-      expect(isDynamic(example)).toBe(true);
-    });
+  it.each([
+    '__BVID__71',
+    'StyledBox-sc-13pk1d4-0',
+    'gLFyf',
+    'gb_C',
+    'TSPr2b',
+    'intercom-123v9c3',
+    'u_0_b',
+    'ggWlfB2BMlWvNeAo2F0uqw',
+  ])('returns true for dynamic value %s', (example) => {
+    expect(isDynamic(example)).toBe(true);
   });
 
-  it('returns false for non-dynamic values', () => {
-    falseExamples.forEach((example) => {
-      expect(isDynamic(example)).toBe(false);
-    });
+  it.each([
+    'toggle',
+    'destroy',
+    'btn',
+    'mat',
+    'form-control',
+    'b-content__page-input',
+    'glide__bullet',
+    'app',
+    'nav-link',
+    'login-button',
+    'col-sm-12',
+    'footer',
+    'intercom-container-body',
+    'new-todo',
+    'inputtext',
+    'email',
+    'learn-bar',
+    'searchInput',
+    'js-lang-list-button',
+    'central-textlogo__image',
+    'footer-sidebar-icon',
+    'svg',
+    'icon',
+    'fa-search',
+    'slider',
+    'btn-playr-primary',
+    'MyCart__itemCount__label',
+    'SearchBox__cover',
+    'tnt__zipInput',
+    'MyAccount__label',
+    'nav-sprite',
+    'desktop-grid-3',
+    'nytslm-li-link',
+  ])('returns false for non-dynamic value %s', (example) => {
+    expect(isDynamic(example)).toBe(false);
   });
 });
