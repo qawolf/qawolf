f2d9332dd05acbee2eb548b81bc7bdfdda2dd238
Eric Dobbertin <eric@dairystatedesigns.com>
2020-09-15 12:34:17 -0500

    test: update tests

--- test/build-workflow/buildClickSteps.test.ts
+++ test/build-workflow/buildClickSteps.test.ts
@@ -17,30 +17,30 @@ describe('buildClickSteps', () => {
 
   it('builds one click per group of mousedown/click events', () => {
     const steps = buildClickSteps(events.login);
-    expect(steps.map((step) => step.event.selector)).toMatchSnapshot();
+    expect(steps.map((step) => (step.event as ElementEvent).selector)).toMatchSnapshot();
   });
 
   it('groups [mousedown, change, click] sequence together', () => {
     const steps = buildClickSteps(events.searchGoogle);
-    expect(steps.map((step) => step.event.selector)).toMatchSnapshot();
+    expect(steps.map((step) => (step.event as ElementEvent).selector)).toMatchSnapshot();
   });
 
   it('prefers clicks on inputs', () => {
     const steps = buildClickSteps(events.radioInputs);
-    expect(steps.map((step) => step.event.selector)).toMatchSnapshot();
+    expect(steps.map((step) => (step.event as ElementEvent).selector)).toMatchSnapshot();
   });
 
   it('skips click triggered by Enter', () => {
     const steps = buildClickSteps(events.login);
 
     // click on login link
-    expect(steps[0].event.target.attrs.href).toEqual('/login');
+    expect((steps[0].event as ElementEvent).target.attrs.href).toEqual('/login');
 
     // click on initial input
-    expect(steps[1].event.selector).toEqual('#username');
+    expect((steps[1].event as ElementEvent).selector).toEqual('#username');
 
     // click logout
-    expect(steps[2].event.selector).toEqual('text=Log out');
+    expect((steps[2].event as ElementEvent).selector).toEqual('text=Log out');
   });
 
   it('skips click on select', () => {
