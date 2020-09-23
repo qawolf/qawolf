3d44a5b015160fc81f9f5de7658445b02f60ddeb
Jon Perl <perl.jonathan@gmail.com>
2020-09-23 12:01:55 -0600

    chore: remove create.e2e.test
    Since playwright@1.4 moved to RPC we no longer have access to the page._targetId and cannot connect to it in another process. This test has minimal value since we unit test everything elsewhere and we are not planning on making many changes to the cli commands.

--- qawolf.config.js
+++ qawolf.config.js
@@ -1,36 +1,5 @@
-const createTemplate = ({ name }) => {
-  return `import { Browser, BrowserContext } from 'playwright';
-import qawolf from 'qawolf';
-import { connect } from '../browser';
-import { TEST_URL } from '../utils';
-
-let browser: Browser;
-let context: BrowserContext;
-
-beforeAll(async () => {
-  browser = await connect();
-  context = await browser.newContext();
-  await qawolf.register(context);
-});
-
-afterAll(async () => {
-  await qawolf.stopVideos();
-  await browser.close();
-});
-
-test("${name}", async () => {
-  const targetPage = await context.newPage();
-  const targetId = (targetPage as any)._delegate._targetId;
-  console.log(\`targetId="\${targetId}"\`);
-  await targetPage.goto(\`\${TEST_URL}infinite-scroll\`);
-  await qawolf.create();
-});`;
-};
-
 module.exports = {
   config: 'node_modules/qawolf/ts-jest.config.json',
-  createTemplate:
-    process.env.QAW_CREATE_E2E_TEST === '1' ? createTemplate : undefined,
   rootDir: 'test/.qawolf',
   useTypeScript: true,
 };
