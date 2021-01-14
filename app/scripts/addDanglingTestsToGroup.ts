701f8f8d86d474a4fced0d6a3b5ab433c0aa1a91
Jon Perl <perl.jonathan@gmail.com>
2021-01-14 11:28:30 -0700

    add useful script

---
+++ app/scripts/addDanglingTestsToGroup.ts
@@ -0,0 +1,39 @@
+import { db } from "../server/db";
+import { Logger } from "../server/Logger";
+import { createGroupTest } from "../server/models/group_test";
+
+const logger = new Logger();
+
+export const addDanglingTestsToGroup = async ({
+  creator_id,
+  group_id,
+}: {
+  creator_id: string;
+  group_id: string;
+}): Promise<void> => {
+  const result = await db.raw(`
+  SELECT id FROM tests
+  WHERE creator_id = '${creator_id}' AND deleted_at IS NULL
+  AND NOT EXISTS (
+     SELECT FROM group_tests
+     WHERE  test_id = tests.id
+  )
+  `);
+
+  console.log("Found danging tests", result.rows.length);
+
+  await db.transaction(async (trx) => {
+    const promises = result.rows.map((row) =>
+      createGroupTest({ group_id, test_id: row.id }, { logger, trx })
+    );
+
+    await Promise.all(promises);
+  });
+
+  console.log("Success!");
+};
+
+addDanglingTestsToGroup({
+  creator_id: "",
+  group_id: "",
+});
