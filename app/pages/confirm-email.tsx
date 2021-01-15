3d423cbab2ec32391464bb79399ce174016abf5b
Laura Cressman <laura.cressman@hey.com>
2021-01-14 19:09:50 -0700

    outline new login code page design

--- app/pages/confirm-email.tsx
+++ app/pages/confirm-email.tsx
@@ -1,16 +1,17 @@
-import { Box, Image } from "grommet";
+import { Box, ThemeContext } from "grommet";
 import { useRouter } from "next/router";
 import { useContext, useEffect } from "react";
 
 import CodeError from "../components/ConfirmEmail/CodeError";
 import CodeInput from "../components/ConfirmEmail/CodeInput";
-import Text from "../components/shared/Text";
+import Logo from "../components/shared-new/icons/Logo";
+import Text from "../components/shared-new/Text";
 import { StateContext } from "../components/StateContext";
 import { UserContext } from "../components/UserContext";
 import { useSignInWithEmail } from "../hooks/mutations";
 import { routes } from "../lib/routes";
 import { copy } from "../theme/copy";
-import { edgeSize } from "../theme/theme";
+import { edgeSize, theme } from "../theme/theme-new";
 
 export default function ConfirmEmail(): JSX.Element {
   const { email, signUp } = useContext(StateContext);
@@ -34,23 +35,28 @@ export default function ConfirmEmail(): JSX.Element {
   };
 
   return (
-    <Box align="center" margin={{ horizontal: "medium", top: "xxlarge" }}>
-      <Image src="logo192.png" width={edgeSize.xxlarge} />
-      <Text
-        color="black"
-        margin={{ vertical: "medium" }}
-        size="xlarge"
-        textAlign="center"
-        weight="bold"
-      >
-        {copy.checkEmail}
-      </Text>
-      <Text color="black" size="large" textAlign="center">
-        {copy.loginCodeSent}
-        <b>{email}</b>:
-      </Text>
-      <CodeInput error={error?.message} onSubmit={handleSubmit} />
-      <CodeError error={error?.message} />
-    </Box>
+    <ThemeContext.Extend value={theme}>
+      <Box align='center' background='white' justify='center' overflow={{ vertical: 'auto' }} style={{ minHeight: '100vh' }}>
+        <Logo width={edgeSize.xxxlarge} />
+        <Text
+          color="textDark"
+          margin={{ bottom: 'xxsmall', top: "large" }}
+          size="medium"
+          weight="bold"
+        >
+          {copy.checkEmail}
+        </Text>
+        <Box align='center' direction='row'>
+          <Text color="textLight" size="xsmall" weight='normal'>
+            {copy.loginCodeSent}
+          </Text>
+          <Text color="textDark" margin={{left: 'xxxsmall'}} size="xsmall" weight='medium'>
+            {email}
+          </Text>
+        </Box>
+        {/* <CodeInput error={error?.message} onSubmit={handleSubmit} />
+      <CodeError error={error?.message} /> */}
+      </Box>
+    </ThemeContext.Extend>
   );
 }
