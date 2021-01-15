5d0f12aa053dd03707da28d674372b67791fc2e2
Laura Cressman <laura.cressman@hey.com>
2021-01-14 18:51:35 -0700

    add tests

---
+++ app/test/lib/helpers.test.ts
@@ -0,0 +1,36 @@
+import { DEFAULT_URL, isValidURL, parseUrl } from "../../lib/helpers";
+
+describe("isValidURL", () => {
+  it("returns true if URL is valid", () => {
+    expect(isValidURL("https://news.ycombinator.com/")).toBe(true);
+    expect(isValidURL("http://localhost:3000")).toBe(true);
+    expect(isValidURL("http://en.wikipedia.org/wiki/Template:Welcome")).toBe(
+      true
+    );
+  });
+
+  it("returns false if URL is not valid", () => {
+    expect(isValidURL("invalid")).toBe(false);
+    expect(isValidURL("javascript:void(0)")).toBe(false);
+    expect(isValidURL("www.example.com")).toBe(false);
+  });
+});
+
+describe("parseUrl", () => {
+  it("returns default URL if no URL specified", () => {
+    expect(parseUrl("")).toBe(DEFAULT_URL);
+  });
+
+  it("returns URL if protocol included", () => {
+    expect(parseUrl("https://news.ycombinator.com")).toBe(
+      "https://news.ycombinator.com"
+    );
+  });
+
+  it("adds protocol if not included", () => {
+    expect(parseUrl("localhost:3000")).toBe("http://localhost:3000");
+    expect(parseUrl("news.ycombinator.com")).toBe(
+      "https://news.ycombinator.com"
+    );
+  });
+});
