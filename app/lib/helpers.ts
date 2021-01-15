a8f3dbb508d8212def3681e45a85ad9ff02c4b49
Laura Cressman <laura.cressman@hey.com>
2021-01-14 17:39:12 -0700

    allow URLs with colons

--- app/lib/helpers.ts
+++ app/lib/helpers.ts
@@ -77,24 +77,13 @@ export const formatTimestamp = (timestamp: string): string => {
 };
 
 export const isValidURL = (url: string): boolean => {
-  if (
-    url.startsWith("http://localhost") ||
-    url.startsWith("https://localhost")
-  ) {
-    return true;
+  try {
+    const parsed = new URL(url);
+
+    return ["https:", "http:"].includes(parsed.protocol);
+  } catch (error) {
+    return false;
   }
-
-  const pattern = new RegExp(
-    "^(https?:\\/\\/)?" + // protocol
-      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-      "(\\#[-a-z\\d_]*)?$",
-    "i"
-  ); // fragment locator
-
-  return !!pattern.test(url);
 };
 
 export const parseUrl = (url: string): string => {
