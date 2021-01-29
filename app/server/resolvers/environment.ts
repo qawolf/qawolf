5d8b147b5d7eea90eb7ebb098cfd759fcad9cb8b
Laura Cressman <laura.cressman@hey.com>
2021-01-28 17:11:46 -0700

    await access checks

--- app/server/resolvers/environment.ts
+++ app/server/resolvers/environment.ts
@@ -36,7 +36,7 @@ export const deleteEnvironmentResolver = async (
   const log = logger.prefix("deleteEnvironmentResolver");
   log.debug("delete", id);
 
-  ensureEnvironmentAccess({ environment_id: id, logger, teams });
+  await ensureEnvironmentAccess({ environment_id: id, logger, teams });
 
   return db.transaction(async (trx) => {
     return deleteEnvironment(id, { logger, trx });
@@ -64,7 +64,7 @@ export const updateEnvironmentResolver = async (
   const log = logger.prefix("updateEnvironmentResolver");
   log.debug("environment", id);
 
-  ensureEnvironmentAccess({ environment_id: id, logger, teams });
+  await ensureEnvironmentAccess({ environment_id: id, logger, teams });
 
   return db.transaction(async (trx) => {
     return updateEnvironment({ id, name }, { logger, trx });
