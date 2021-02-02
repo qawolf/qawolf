22842c81b95b49e45282214de57b1ddee9784bb9
Laura Cressman <laura.cressman@hey.com>
2021-02-01 21:18:01 -0700

    allow resize of code pane

--- app/components/Editor/Sidebar/index.tsx
+++ app/components/Editor/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { Box, ThemeContext } from "grommet";
 import { useState } from "react";
+import { Resizable } from "re-resizable";
 
 import { NavigationOption } from "../../../lib/types";
 import { theme } from "../../../theme/theme-new";
@@ -9,20 +10,41 @@ import Navigation from "./Navigation";
 import RunLogs from "./RunLogs";
 import Buttons from "./Buttons";
 
-const width = "480px";
+const enable = {
+  top: false,
+  right: true,
+  bottom: false,
+  left: false,
+  topRight: false,
+  bottomRight: false,
+  bottomLeft: false,
+  topLeft: false,
+};
+
+const minWidth = 480;
 
 export default function Sidebar(): JSX.Element {
   const [selected, setSelected] = useState<NavigationOption>("code");
 
   return (
     <ThemeContext.Extend value={theme}>
-      <Box background="gray10" flex={false} height="full" width={width}>
-        <Navigation selected={selected} setSelected={setSelected} />
-        {selected === "code" && <CodeEditor />}
-        {selected === "helpers" && <HelpersEditor />}
-        <RunLogs isVisible={selected === "logs"} />
-        <Buttons />
-      </Box>
+      <Resizable
+        defaultSize={{
+          height: "100%",
+          width: minWidth,
+        }}
+        enable={enable}
+        maxWidth="50%"
+        minWidth={minWidth}
+      >
+        <Box background="gray10" flex={false} height="full">
+          <Navigation selected={selected} setSelected={setSelected} />
+          {selected === "code" && <CodeEditor />}
+          {selected === "helpers" && <HelpersEditor />}
+          <RunLogs isVisible={selected === "logs"} />
+          <Buttons />
+        </Box>
+      </Resizable>
     </ThemeContext.Extend>
   );
 }
