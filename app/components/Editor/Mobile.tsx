aa101874fa8ca34eb1a8e92b7ba3c628cbac7568
Laura Cressman <laura.cressman@hey.com>
2021-02-02 20:29:19 -0700

    fix type error

--- app/components/Editor/Mobile.tsx
+++ app/components/Editor/Mobile.tsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import { TestContext } from "./contexts/TestContext";
 import { Mode } from "./hooks/mode";
 import Placeholder from "./Canvas/Placeholder";
-import TestVideo from "./TestVideo";
+import TestVideo from "./Canvas/TestVideo";
 
 type Props = {
   mode: Mode;
