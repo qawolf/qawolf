e115d8e1b6d1cf9aadb5f31b24050931af376168
Laura Cressman <laura.cressman@hey.com>
2021-02-05 15:29:26 -0700

    fix type error

--- app/components/Editor/Canvas/Placeholder.tsx
+++ app/components/Editor/Canvas/Placeholder.tsx
@@ -39,13 +39,6 @@ export default function Placeholder({
     iconHtml = null;
   }
 
-  if (run?.suite_id && !run.started_at) {
-    // show preparing placeholder if suite run has not started
-    message = `${copy.notStarted}...`;
-  } else if (mode === "create") {
-    iconHtml = null;
-    message = "";
-  }
   // ask the user to run the test if there is no runner connected or pending
   else if (
     mode === "test" &&
