a68ec550283e21a413d9a06e111db919ba0200bb
Jon Perl <perl.jonathan@gmail.com>
2021-02-08 18:50:57 -0700

    rename foreign key constraints

--- app/server/migrations/20210208102538_rename_groups_to_triggers.ts
+++ app/server/migrations/20210208102538_rename_groups_to_triggers.ts
@@ -11,16 +11,20 @@ export async function up(knex: Knex): Promise<Knex.SchemaBuilder> {
   await knex.schema.raw(`
     ALTER TABLE group_tests RENAME TO test_triggers;
     ALTER TABLE test_triggers RENAME COLUMN group_id TO trigger_id;
+    ALTER TABLE test_triggers RENAME CONSTRAINT "group_tests_group_id_foreign" TO "test_triggers_trigger_id_foreign";
+    ALTER TABLE test_triggers RENAME CONSTRAINT "group_tests_test_id_foreign" TO "test_triggers_test_id_foreign";
     ALTER INDEX IF EXISTS group_tests_pkey RENAME TO test_triggers_pkey;
     ALTER INDEX IF EXISTS group_tests_group_id_test_id_unique RENAME TO test_triggers_test_id_trigger_id_unique;
 `);
 
   await knex.schema.raw(`
     ALTER TABLE github_commit_statuses RENAME COLUMN group_id TO trigger_id;
+    ALTER TABLE github_commit_statuses RENAME CONSTRAINT "github_commit_statuses_group_id_foreign" TO "github_commit_statuses_trigger_id_foreign";
 `);
 
   await knex.schema.raw(`
     ALTER TABLE suites RENAME COLUMN group_id TO trigger_id;
+    ALTER TABLE suites RENAME CONSTRAINT "suites_group_id_foreign" TO "suites_trigger_id_foreign";
 `);
 }
 
@@ -32,15 +36,19 @@ export async function down(knex: Knex): Promise<Knex.SchemaBuilder> {
   await knex.schema.raw(`
     ALTER TABLE test_triggers RENAME TO group_tests;
     ALTER TABLE group_tests RENAME COLUMN trigger_id TO group_id;
+    ALTER TABLE test_triggers RENAME CONSTRAINT "test_triggers_trigger_id_foreign" TO "group_tests_group_id_foreign";
+    ALTER TABLE test_triggers RENAME CONSTRAINT "test_triggers_test_id_foreign" TO "group_tests_test_id_foreign";
     ALTER INDEX IF EXISTS test_triggers_pkey RENAME TO group_tests_pkey;
     ALTER INDEX IF EXISTS test_triggers_test_id_trigger_id_unique RENAME TO group_tests_group_id_test_id_unique;
 `);
 
   await knex.schema.raw(`
     ALTER TABLE github_commit_statuses RENAME COLUMN trigger_id TO group_id;
+    ALTER TABLE github_commit_statuses RENAME CONSTRAINT "github_commit_statuses_trigger_id_foreign" TO "github_commit_statuses_group_id_foreign";
 `);
 
   await knex.schema.raw(`
     ALTER TABLE suites RENAME COLUMN trigger_id TO group_id;
+    ALTER TABLE suites RENAME CONSTRAINT "suites_trigger_id_foreign" TO "suites_group_id_foreign";
 `);
 }
