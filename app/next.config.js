36fc128e0cc7456ad9c53edf88b54864fcb64033
Jon Perl <perl.jonathan@gmail.com>
2021-02-11 18:13:36 -0700

    add netlify

--- app/next.config.js
+++ app/next.config.js
@@ -4,12 +4,34 @@ const withMDX = require("@next/mdx")({
 
 const config = withMDX({
   pageExtensions: ["ts", "tsx", "md", "mdx"],
-  webpack: (config) => {
+  webpack: (config, { webpack }) => {
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
       loader: "@graphql-tools/webpack-loader",
     });
+
+    if (process.env.NETLIFY) {
+      // const nodeExternals = require("webpack-node-externals");
+      // config.externals.push(nodeExternals());
+
+      // https://community.netlify.com/t/unable-to-publish-function-using-postgres/12293/2
+      // https://github.com/knex/knex/issues/1446#issuecomment-253245823
+      config.plugins.push(
+        ...[
+          new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
+          new webpack.IgnorePlugin(/mssql/, /\/knex\//),
+          new webpack.IgnorePlugin(/mysql/, /\/knex\//),
+          new webpack.IgnorePlugin(/mysql2/, /\/knex\//),
+          new webpack.IgnorePlugin(/oracle/, /\/knex\//),
+          new webpack.IgnorePlugin(/oracledb/, /\/knex\//),
+          new webpack.IgnorePlugin(/pg-native/, /\/pg\//),
+          new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
+          new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
+          new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//),
+        ]
+      );
+    }
 
     return config;
   },
@@ -22,4 +44,5 @@ module.exports = {
   experimental: {
     productionBrowserSourceMaps: true,
   },
+  target: "serverless",
 };
