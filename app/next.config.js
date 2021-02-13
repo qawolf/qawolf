04cb751f20d2872131a2678f1b710ecc8ecfb423
Jon Perl <perl.jonathan@gmail.com>
2021-02-13 14:47:00 -0700

    fix netlify

--- app/next.config.js
+++ app/next.config.js
@@ -12,25 +12,21 @@ const config = withMDX({
     });
 
     if (process.env.NETLIFY) {
-      // const nodeExternals = require("webpack-node-externals");
-      // config.externals.push(nodeExternals());
-
-      // https://community.netlify.com/t/unable-to-publish-function-using-postgres/12293/2
       // https://github.com/knex/knex/issues/1446#issuecomment-253245823
       config.plugins.push(
         ...[
-          new webpack.IgnorePlugin(/mariasql/, /\/knex\//),
-          new webpack.IgnorePlugin(/mssql/, /\/knex\//),
-          new webpack.IgnorePlugin(/mysql/, /\/knex\//),
-          new webpack.IgnorePlugin(/mysql2/, /\/knex\//),
-          new webpack.IgnorePlugin(/oracle/, /\/knex\//),
-          new webpack.IgnorePlugin(/oracledb/, /\/knex\//),
-          new webpack.IgnorePlugin(/pg-native/, /\/pg\//),
-          new webpack.IgnorePlugin(/pg-query-stream/, /\/knex\//),
-          new webpack.IgnorePlugin(/sqlite3/, /\/knex\//),
-          new webpack.IgnorePlugin(/strong-oracle/, /\/knex\//),
-        ]
+          /mariasql/,
+          /mssql/,
+          /mysql/,
+          /mysql2/,
+          /oracle/,
+          /oracledb/,
+          /pg-query-stream/,
+          /sqlite3/,
+          /strong-oracle/,
+        ].map((pattern) => new webpack.IgnorePlugin(pattern, /\/knex\//))
       );
+      config.plugins.push(new webpack.IgnorePlugin(/pg-native/, /\/pg\//));
     }
 
     return config;
--- app/pages/api/graphql.ts
+++ app/pages/api/graphql.ts
@@ -1,41 +1,14 @@
-import {
-  ApolloServerPluginUsageReporting,
-  PluginDefinition,
-} from "apollo-server-core";
+import { PluginDefinition } from "apollo-server-core";
 import { ApolloServer } from "apollo-server-micro";
+import { NextApiRequest, NextApiResponse } from "next";
 
 import { context } from "../../server/context";
+import { connectDb } from "../../server/db";
 import { CLIENT_ERROR_CODES, ClientError } from "../../server/errors";
 import { resolvers } from "../../server/resolvers";
 import * as typeDefs from "../../server/schema.graphql";
-import { Context } from "../../server/types";
 
-const plugins: PluginDefinition[] = [
-  {
-    requestDidStart: () => ({
-      willSendResponse: ({ context }) => {
-        const { db } = context as Context;
-        if (db) db.destroy();
-      },
-    }),
-  },
-];
-
-if (process.env.APOLLO_KEY) {
-  plugins.push(
-    ApolloServerPluginUsageReporting({
-      rewriteError(err) {
-        const { code } = err?.extensions || {};
-
-        // do not report client errors
-        if (CLIENT_ERROR_CODES.includes(code)) return null;
-
-        // report all unexpected errors
-        return err;
-      },
-    })
-  );
-}
+const plugins: PluginDefinition[] = [];
 
 const apolloServer = new ApolloServer({
   context,
@@ -57,4 +30,17 @@ const apolloServer = new ApolloServer({
 // https://nextjs.org/docs/api-routes/api-middlewares#custom-config
 export const config = { api: { bodyParser: false } };
 
-export default apolloServer.createHandler({ path: "/api/graphql" });
+const graphqlHandler = apolloServer.createHandler({ path: "/api/graphql" });
+
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const db = connectDb();
+  (req as any).db = db;
+
+  await graphqlHandler(req, res);
+
+  await db.destroy();
+}
