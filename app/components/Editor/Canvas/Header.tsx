3479bd4f21cf3afce2bc7585d6eb9d200a368b05
Laura Cressman <laura.cressman@hey.com>
2021-02-16 10:07:07 -0700

    remove browser icon from header

--- app/components/Editor/Canvas/Header.tsx
+++ app/components/Editor/Canvas/Header.tsx
@@ -1,30 +1,19 @@
 import { Box } from "grommet";
 
-import { copy } from "../../../theme/copy";
-import { colors, edgeSize } from "../../../theme/theme-new";
+import { edgeSize } from "../../../theme/theme-new";
 import { borderSize } from "../../../theme/theme-new";
-import Browser from "../../shared-new/icons/Browser";
-import Text from "../../shared-new/Text";
 import CodeToggle from "./CodeToggle";
 
 export default function Header(): JSX.Element {
   return (
     <Box
-      align="center"
       border={{ color: "gray3", side: "bottom", size: borderSize.xsmall }}
-      direction="row"
       flex={false}
       height={`calc(20px + (2 * ${edgeSize.small}))`} // height of code toggle
-      justify="between"
+      justify="center"
       pad="small"
     >
       <CodeToggle />
-      <Box align="center" direction="row">
-        <Browser color={colors.gray9} size={edgeSize.small} />
-        <Text color="gray9" margin={{ left: "xxsmall" }} size="component">
-          {copy.browser}
-        </Text>
-      </Box>
     </Box>
   );
 }
