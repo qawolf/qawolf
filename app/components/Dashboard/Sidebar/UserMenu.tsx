59704313bb9adbb9b2b8797a06160ff165ae73e1
Laura Cressman <laura.cressman@hey.com>
2021-02-22 15:44:46 -0700

    log out

--- app/components/Dashboard/Sidebar/UserMenu.tsx
+++ app/components/Dashboard/Sidebar/UserMenu.tsx
@@ -1,6 +1,14 @@
+import { useRouter } from "next/router";
 import { useContext } from "react";
 
+import { resetIntercom } from "../../../hooks/intercom";
+import { JWT_KEY } from "../../../lib/client";
+import { routes } from "../../../lib/routes";
+import { updateSentryUser } from "../../../lib/sentry";
 import { state } from "../../../lib/state";
+import { copy } from "../../../theme/copy";
+import Divider from "../../shared-new/Divider";
+import LogOut from "../../shared-new/icons/LogOut";
 import Menu from "../../shared-new/Menu";
 import Option from "../../shared-new/Select/Option";
 import { StateContext } from "../../StateContext";
@@ -12,10 +20,20 @@ type Props = {
 };
 
 export default function UserMenu({ isOpen, onClose }: Props): JSX.Element {
+  const { replace } = useRouter();
+
   const { teamId } = useContext(StateContext);
   const { user } = useContext(UserContext);
 
   if (!isOpen || !user) return null;
+
+  const handleLogOutClick = () => {
+    localStorage.removeItem(JWT_KEY);
+    state.clear();
+    resetIntercom();
+    updateSentryUser({ email: null });
+    replace(routes.home);
+  };
 
   const handleTeamClick = (teamId: string): void => {
     state.setTeamId(teamId);
@@ -33,5 +51,15 @@ export default function UserMenu({ isOpen, onClose }: Props): JSX.Element {
     );
   });
 
-  return <Menu direction="down">{optionsHtml}</Menu>;
+  return (
+    <Menu direction="down">
+      {optionsHtml}
+      <Divider margin={{ vertical: "xxxsmall" }} />
+      <Option
+        IconComponent={LogOut}
+        label={copy.logOut}
+        onClick={handleLogOutClick}
+      />
+    </Menu>
+  );
 }
