bb9ed6de68679ee1dae8931c26d9a55cdba25241
Laura Cressman <laura.cressman@hey.com>
2021-03-04 15:20:27 -0700

    remove posthog

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -15,8 +15,7 @@ const description =
 const docSearchJSVersion = "2.6.3";
 
 const intercomAppId = process.env.NEXT_PUBLIC_INTERCOM_APP_ID;
-const postHogApiHost = process.env.NEXT_PUBLIC_POSTHOG_API_HOST;
-const postHogApiKey = process.env.NEXT_PUBLIC_POSTHOG_API_KEY;
+const segmentAppId = process.env.NEXT_PUBLIC_SEGMENT_APP_ID;
 
 class MyDocument extends Document {
   // https://github.com/vercel/next.js/blob/canary/examples/with-grommet/pages/_document.js
@@ -60,13 +59,15 @@ class MyDocument extends Document {
           />
           <meta name="description" content={description} />
           <meta name="twitter:description" content={description} />
-          {/* PostHog */}
-          {!!postHogApiHost && !!postHogApiKey && (
+          {/* Segment */}
+          {!!segmentAppId && (
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-                !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-                posthog.init('${postHogApiKey}',{api_host:'${postHogApiHost}'})
+                !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="bo2cbBG0TD9ZYpY8Vj0W60tStkN32Axp";analytics.SNIPPET_VERSION="4.13.2";
+                analytics.load("${segmentAppId}");
+                analytics.page();
+                }}();
             `,
               }}
             />
