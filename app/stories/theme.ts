b8ce6796d15b8ddaf040a8698377959cd3d5d781
Laura Cressman <laura.cressman@hey.com>
2021-03-05 10:36:36 -0700

    improve comment

--- app/stories/theme.ts
+++ app/stories/theme.ts
@@ -1,9 +1,9 @@
 import { edgeSize, theme } from "../theme/theme-new";
 
+// make grommet play nice with storybook
 export default {
   ...theme,
   box: {
-    // prevent default grommet styling on mobile as we handle manually
     responsiveBreakpoint: null,
   },
   icon: {
