2d3866be47f977994a4408b1d5c020756cc1da91
Jon Perl <perl.jonathan@gmail.com>
2021-03-08 19:17:39 -0700

    CodeUpdater: add frame fill e2e test

--- runner/test/code/CodeUpdater.test.ts
+++ runner/test/code/CodeUpdater.test.ts
@@ -1,22 +1,42 @@
 import { CodeUpdater } from "../../src/code/CodeUpdater";
-import { launch, setBody, sleep } from "../utils";
+import {
+  FixturesServer,
+  launch,
+  LaunchResult,
+  serveFixtures,
+  sleep,
+} from "../utils";
 
-it("updates code", async () => {
-  const { browser, context, page } = await launch();
+let launched: LaunchResult;
+let server: FixturesServer;
+let updater: CodeUpdater;
 
-  await setBody(page, "<button>Hello</button>");
+beforeAll(async () => {
+  launched = await launch();
+  server = await serveFixtures();
 
-  const updater = new CodeUpdater({ page });
-  updater.setContext(context);
-
-  updater.updateCode({
-    code: "// 🐺 QA Wolf will create code here",
-    version: 1,
-  });
+  updater = new CodeUpdater({ page: launched.page });
+  updater.setContext(launched.context);
 
   await updater.enable();
 
-  await page.click("button");
+  await launched.page.goto(`${server.url}/CodeUpdater`);
+});
+
+beforeEach(() => {
+  updater.updateCode({
+    code: "// 🐺 QA Wolf will create code here",
+    version: Date.now(),
+  });
+});
+
+afterAll(async () => {
+  server.close();
+  await launched.browser.close();
+});
+
+it("updates code for a click", async () => {
+  await launched.page.click("button");
 
   // let events propagate
   await sleep(0);
@@ -27,6 +47,23 @@ it("updates code", async () => {
     "await page.click(\\"text=Hello\\");
     // 🐺 QA Wolf will create code here"
   `);
+});
 
-  await browser.close();
+it("updates code for a fill inside a frame", async () => {
+  const frame = await (
+    await launched.page.waitForSelector('[data-qa="frame"]')
+  ).contentFrame();
+
+  await frame!.fill("input", "hello");
+
+  // let events propagate
+  await sleep(1000);
+
+  // we do not expect the code to create the page
+  // since we enabled the updater after those steps
+  expect(updater._code).toMatchInlineSnapshot(`
+    "const frame = await (await page.waitForSelector('[data-qa=\\"frame\\"]')).contentFrame();
+    await frame.fill('[type=\\"text\\"]', \\"hello\\");
+    // 🐺 QA Wolf will create code here"
+  `);
 });
