704c3596e3ccbe013ac32fa276d63e5b919675f1
Laura Cressman <laura.cressman@hey.com>
2021-04-01 20:18:17 -0600

    checkout success page

---
+++ app/pages/checkout-success.tsx
@@ -0,0 +1,47 @@
+import { Box } from "grommet";
+import { useContext } from "react";
+import Confetti from "react-confetti";
+
+import Button from "../components/shared/AppButton";
+import WolfParty from "../components/shared/icons/WolfParty";
+import Text from "../components/shared/Text";
+import { UserContext } from "../components/UserContext";
+import { useEnsureUser } from "../hooks/ensureUser";
+import { useWindowSize } from "../hooks/windowSize";
+import { routes } from "../lib/routes";
+import { copy } from "../theme/copy";
+import { colors } from "../theme/theme";
+
+const confettiColors = [
+  colors.codeBlue,
+  colors.codePink,
+  colors.codePurple,
+  colors.danger5,
+  colors.darkYellow,
+  colors.primary,
+  colors.success5,
+  colors.teal,
+];
+
+export default function CheckoutSuccess(): JSX.Element {
+  useEnsureUser();
+  const { height, width } = useWindowSize();
+
+  const { wolf } = useContext(UserContext);
+
+  return (
+    <Box align="center" margin={{ top: "xxlarge" }}>
+      <Confetti colors={confettiColors} height={height} width={width} />
+      {!!wolf && <WolfParty color={wolf.variant} />}
+      <Text
+        color="gray9"
+        margin={{ bottom: "medium", top: "small" }}
+        size="componentHeader"
+        textAlign="center"
+      >
+        {copy.checkoutSuccess}
+      </Text>
+      <Button href={routes.tests} label={copy.backToDashboard} type="primary" />
+    </Box>
+  );
+}
--- app/theme/copy.ts
+++ app/theme/copy.ts
@@ -51,6 +51,7 @@ export const copy = {
   branchesPlaceholder: "Example: develop,main",
   cancel: "Cancel",
   checkEmail: "Check your email for a code",
+  checkoutSuccess: "🎉 You subscribed to the business plan!",
   chooseElement: "Choose element",
   chooseElementDetail:
     "Select an element, then add a snippet with your chosen action and selector.",
