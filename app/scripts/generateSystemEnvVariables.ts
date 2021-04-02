202c2956e09b014f78b989915c4a7bda01c839eb
Jon Perl <perl.jonathan@gmail.com>
2021-04-02 16:29:20 -0600

    revert script change

--- app/scripts/generateSystemEnvVariables.ts
+++ app/scripts/generateSystemEnvVariables.ts
@@ -5,33 +5,14 @@ import { readFile } from "./generateTypesFile";
 const generateSystemEnvVariables = async (): Promise<void> => {
   const db = connectDb();
 
-  // const privateKey = JSON.stringify(readFile("./qawolf-dev.pem"));
+  const privateKey = JSON.stringify(readFile("./qawolf-dev.pem"));
 
   await db("environment_variables").insert([
-    // {
-    //   id: cuid(),
-    //   is_system: true,
-    //   name: "GITHUB_APP_PRIVATE_KEY",
-    //   value: privateKey,
-    // },
     {
       id: cuid(),
       is_system: true,
-      name: "RUNNER_LOCATIONS",
-      value: JSON.stringify({
-        westus2: {
-          buffer: 3,
-          latitude: 47.233,
-          longitude: -119.852,
-          reserved: 3,
-        },
-        eastus2: {
-          buffer: 2,
-          latitude: 36.6681,
-          longitude: -78.3889,
-          reserved: 0,
-        },
-      }),
+      name: "GITHUB_APP_PRIVATE_KEY",
+      value: privateKey,
     },
   ]);
 
