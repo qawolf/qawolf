b60aba3faa330aa31a9a9fb0930bb5ea65166ea6
Jon Perl <perl.jonathan@gmail.com>
2021-04-05 08:15:00 -0600

    fix migration

--- app/server/migrations/20210404151128_update_emails_is_outbound.ts
+++ app/server/migrations/20210404151128_update_emails_is_outbound.ts
@@ -4,9 +4,6 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable("emails", (table) => {
     table.boolean("is_outbound").notNullable().defaultTo(false);
 
-    table.text("html").nullable().alter();
-    table.text("text").nullable().alter();
-
     table.dropColumn("updated_at");
   });
 }
@@ -14,9 +11,6 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.alterTable("emails", (table) => {
     table.dropColumn("is_outbound");
-
-    table.text("html").notNullable().alter();
-    table.text("text").notNullable().alter();
 
     table.timestamp("updated_at").defaultTo(knex.fn.now()).notNullable();
   });
