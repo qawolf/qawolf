dea5428cdd1ac348f523a74e5dddaf08ae5584eb
Laura Cressman <laura.cressman@hey.com>
2021-04-08 13:40:24 -0700

    test cli command

--- qawolf/src/cli/runTests.ts
+++ qawolf/src/cli/runTests.ts
@@ -1,20 +1,82 @@
+import retry from "async-retry";
 import axios from "axios";
-import { bold, red } from "kleur";
+import { bold, green, red } from "kleur";
 
-type RunTests = {
+type CreateSuite = {
   env?: string;
   triggerId: string;
-  wait: boolean;
 };
 
-const getApiKey = (): string => {
-  const apiKey = process.env.QAWOLF_API_KEY;
-  if (!apiKey) {
+type RunTests = CreateSuite & { wait: boolean };
+
+type Status = "fail" | "pass";
+
+const qaWolfUrl = "https://www.qawolf.com";
+
+const createSuite = async ({
+  env,
+  triggerId,
+}: CreateSuite): Promise<string> => {
+  const suitesUrl = new URL("/api/suites", qaWolfUrl).href;
+
+  const {
+    data: { id, url },
+  } = await axios.post(
+    suitesUrl,
+    { env, trigger_id: triggerId },
+    { headers: { authorization: process.env.QAWOLF_API_KEY } }
+  );
+
+  console.log("Created suite, see details at", url);
+
+  return id;
+};
+
+const ensureApiKey = (): void => {
+  if (!process.env.QAWOLF_API_KEY) {
     console.log(red("Error: must set QAWOLF_API_KEY environment variable"));
+
     process.exit(1);
   }
+};
 
-  return apiKey;
+const waitForSuite = async (suiteId: string): Promise<Status> => {
+  console.log("waiting for tests to run...");
+
+  const suiteUrl = new URL(`/api/suites/${suiteId}`, qaWolfUrl).href;
+
+  const intervalMs = 10 * 1000; // 10 seconds
+  const timeoutMs = 30 * 60 * 1000; // 30 minutes
+  let timeout = false;
+
+  const requestPromise = retry(
+    async () => {
+      if (timeout) return;
+
+      const { data } = await axios.get(suiteUrl, {
+        headers: { authorization: process.env.QAWOLF_API_KEY },
+      });
+
+      if (!data.is_complete) throw new Error("suite not complete");
+
+      return data.status;
+    },
+    {
+      factor: 1,
+      maxTimeout: intervalMs,
+      minTimeout: intervalMs,
+      retries: Math.round(timeoutMs / intervalMs),
+    }
+  );
+
+  const timeoutPromise = new Promise<Status>((_, reject) => {
+    setTimeout(() => {
+      timeout = true;
+      reject(new Error("suite not complete"));
+    }, timeoutMs);
+  });
+
+  return Promise.race([requestPromise, timeoutPromise]);
 };
 
 export default async function runTests({
@@ -24,9 +86,20 @@ export default async function runTests({
 }: RunTests): Promise<void> {
   console.log(bold(`\n🐺  Run QA Wolf tests for trigger ${triggerId}`));
 
-  console.log("VARIABLES", env, typeof env);
-  console.log("TRIGGER", triggerId, "WAIT", wait);
+  ensureApiKey();
+  const suiteId = await createSuite({ env, triggerId });
 
-  const apiKey = getApiKey();
-  //   process.exit(1);
+  if (!wait) {
+    console.log("🐺 complete, do not wait for tests to run");
+    return;
+  }
+
+  const status = await waitForSuite(suiteId);
+
+  const colorFn = status === "fail" ? red : green;
+  console.log(colorFn(`🐺 complete, suite ${status}ed`));
+
+  if (status === "pass") return;
+
+  process.exit(1);
 }
