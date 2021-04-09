4094ed38596e2c170e8174804bd94b2bbcaa9375
Laura Cressman <laura.cressman@hey.com>
2021-04-08 20:17:49 -0700

    complete doc

--- app/components/Modals/Triggers/CreateOrEditTrigger/ApiFields.tsx
+++ app/components/Modals/Triggers/CreateOrEditTrigger/ApiFields.tsx
@@ -26,7 +26,8 @@ export default function ApiFields({ editTriggerId }: Props): JSX.Element {
   const apiCall = `curl -H "Authorization: ${apiKey}" -H "Content-Type: application/json" https://www.qawolf.com/api/suites -d '{"env": { "MY_VARIABLE": "secret" }, "trigger_id": "${triggerId}"}'`;
   const apiDocsHref = buildQaWolfDocsLink("/run-tests-with-api");
 
-  const cliCall = `npx qawolf test --trigger ${triggerId} --env '{"env": { "MY_VARIABLE": "secret" } }'`;
+  const cliCall = `QAWOLF_API_KEY: ${apiKey}`;
+  const cliCall2 = `npx qawolf test --trigger ${triggerId} --env '{"env": { "MY_VARIABLE": "secret" } }'`;
   const cliDocsHref = buildQaWolfDocsLink("/run-tests-with-cli");
 
   return (
@@ -51,6 +52,7 @@ export default function ApiFields({ editTriggerId }: Props): JSX.Element {
         </Text>
       </Box>
       <CodeBlock>{cliCall}</CodeBlock>
+      <CodeBlock margin={{ top: "xxsmall" }}>{cliCall2}</CodeBlock>
     </>
   );
 }
