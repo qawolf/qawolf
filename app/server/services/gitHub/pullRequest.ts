e5243a69adb454f88ae49895af6f18e96f011769
Laura Cressman <laura.cressman@hey.com>
2021-04-15 13:05:13 -0700

    function to update existing comment

--- app/server/services/gitHub/pullRequest.ts
+++ app/server/services/gitHub/pullRequest.ts
@@ -8,19 +8,45 @@ export type PullRequestComment = RestEndpointMethodTypes["issues"]["createCommen
 type CreatePullRequestComment = {
   body: string;
   installationId: number;
-  issue_number: number;
+  issueNumber: number;
+  owner: string;
+  repo: string;
+};
+
+type UpdatePullRequestComment = {
+  body: string;
+  commentId: number;
+  installationId: number;
   owner: string;
   repo: string;
 };
 
 export const createPullRequestComment = async (
-  { installationId, ...fields }: CreatePullRequestComment,
+  { installationId, issueNumber, ...fields }: CreatePullRequestComment,
   options: ModelOptions
 ): Promise<PullRequestComment> => {
   const token = await createInstallationAccessToken(installationId, options);
   const octokit = new Octokit({ auth: token });
 
-  const { data } = await octokit.issues.createComment({ ...fields });
+  const { data } = await octokit.issues.createComment({
+    ...fields,
+    issue_number: issueNumber,
+  });
+
+  return data;
+};
+
+export const updatePullRequestComment = async (
+  { commentId, installationId, ...fields }: UpdatePullRequestComment,
+  options: ModelOptions
+): Promise<PullRequestComment> => {
+  const token = await createInstallationAccessToken(installationId, options);
+  const octokit = new Octokit({ auth: token });
+
+  const { data } = await octokit.issues.updateComment({
+    ...fields,
+    comment_id: commentId,
+  });
 
   return data;
 };
