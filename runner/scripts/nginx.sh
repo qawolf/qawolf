6fe6426cbec9c2d78efbb08b3c0355c5be9d9202
Jeremy Shimko <jeremy.shimko@gmail.com>
2021-05-03 13:13:39 -0400

    add dedicated WORKDIR in runner docker build

--- runner/scripts/nginx.sh
+++ runner/scripts/nginx.sh
@@ -2,13 +2,13 @@
 
 if [[ -v QAWOLF_RUNNER_ID ]]
 then
-    . /scripts/ssl/nginx.sh
+    . /opt/runner/scripts/ssl/nginx.sh
 else
   echo "configure local nginx"
 
   # Replace DNS_SERVER and set up config
   export DNS_SERVER=$(cat /etc/resolv.conf |grep -i '^nameserver'|head -n1|cut -d ' ' -f2)
-  envsubst '${DNS_SERVER}' < /scripts/nginx.conf > /etc/nginx/nginx.conf
+  envsubst '${DNS_SERVER}' < /opt/runner/scripts/nginx.conf > /etc/nginx/nginx.conf
 fi
 
 # Start nginx with daemon off
