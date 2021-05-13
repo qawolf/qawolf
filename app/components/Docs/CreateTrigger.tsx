4a79b46ad10a41cbc7e56b83318fed85d5b19508
Laura Cressman <laura.cressman@hey.com>
2021-05-13 15:13:06 -0700

    update run tests on schedule doc

--- app/components/Docs/CreateTrigger.tsx
+++ app/components/Docs/CreateTrigger.tsx
@@ -11,34 +11,17 @@ export default function CreateTrigger({ description }: Props): JSX.Element {
         triggers to determine when to run your tests.`}
       </p>
       <p>
-        You can manage triggers from the dashboard or from the test editor. The
-        button to open the triggers popup is in the top right corner of the test
-        editor and in the sidebar of the{" "}
+        To manage your team's triggers, use the "Triggers" button in the sidebar
+        of the{" "}
         <ExternalLink href="https://www.qawolf.com/tests">
           dashboard
         </ExternalLink>
         .
       </p>
       <Image
-        alt="Add trigger from test editor"
-        height={221}
-        src="/docs/run-tests-on-a-schedule/add-trigger.png"
-        width={1600}
-      />
-      <Image
         alt="Add trigger from dashboard"
-        height={353}
+        height={328}
         src="/docs/run-tests-on-a-schedule/add-trigger-dashboard.png"
-        width={1600}
-      />
-      <p>
-        When tests are selected in the dashboard, the edit triggers button also
-        appears in the top row.
-      </p>
-      <Image
-        alt="Add trigger from dashboard with tests selected"
-        height={353}
-        src="/docs/run-tests-on-a-schedule/add-trigger-dashboard-selected.png"
         width={1600}
       />
       <p>Click this button to open a popup with options to create a trigger.</p>
